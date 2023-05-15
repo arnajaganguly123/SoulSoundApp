1c92da8acb92555d010e06ee5405992e0ebf5e50
Arnaja Ganguly <79324117+arnajaganguly123@users.noreply.github.com>
2023-05-15 10:10:05 +0530

    Add files via upload

---
+++ Soul Sound App/SoulSoundApp/app/src/main/java/com/example/soulsoundapp/Signuppage.java
@@ -0,0 +1,61 @@
+package com.example.soulsoundapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Signuppage extends AppCompatActivity {
+
+    private EditText mEmail;
+    private EditText mPass;
+    private Button btnReg;
+    private TextView mSignin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signuppage);
+
+        registration();
+    }
+
+    private void registration(){
+
+        mEmail=findViewById(R.id.email_reg);
+        mPass=findViewById(R.id.password_login);
+        btnReg=findViewById(R.id.btn_reg);
+        mSignin=findViewById(R.id.signin_here);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email=mEmail.getText().toString().trim();
+                String pass=mPass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email Required..");
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)){
+                    mPass.setError("Password Required..");
+                    return;
+                }
+
+            }
+        });
+
+        mSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+}
